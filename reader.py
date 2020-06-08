cdcb51f57184809654425dba5a11080397d846bb
Federico Scutti <fscutti@6400L-129535-M.local>
2020-06-08 16:42:40 +1000

    some more details to the reader

--- reader.py
+++ reader.py
@@ -1,7 +1,9 @@
+# python 3.8.3 needed
 import yaml
 
-stream = open("data_preparation.yaml", "r")
-#dictionary = yaml.load(stream, Loader=yaml.FullLoader)
+#stream = open("data_preparation.yaml", "r")
+#stream = open("reconstruction.yaml", "r")
+stream = open("analysis.yaml", "r")
 dictionary = yaml.full_load(stream)
 for key, value in dictionary.items():
     print(key + " : " + str(value))
