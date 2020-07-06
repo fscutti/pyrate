ef3600bd65f22e8a7e577fdb82952370629dbf41
Federico Scutti <fscutti@6400L-129535-M.local>
2020-07-06 11:09:42 +1000

    some configuration updates

--- reader.py
+++ reader.py
@@ -3,7 +3,7 @@ import yaml
 
 #stream = open("data_preparation.yaml", "r")
 #stream = open("reconstruction.yaml", "r")
-stream = open("analysis.yaml", "r")
+stream = open("/Users/fscutti/pyrate/config/algorithms.yaml", "r")
 dictionary = yaml.full_load(stream)
 for key, value in dictionary.items():
     print(key + " : " + str(value))
