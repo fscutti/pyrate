6a7f76d43ddd54d81cbc2287b41a9a9ddff68bdd
Federico Scutti <fscutti@6400L-129535-M.local>
2020-07-17 18:01:15 +1000

    add comments

--- pyrate/core/Job.py
+++ pyrate/core/Job.py
@@ -17,18 +17,27 @@ class Job:
     def setup(self):
         """ Initialise 'private' data members.
         """
-        #self.build_run_config()  
         # list of outputs
         # list of algorithms
         # 
         
-        inputs = {}
+        self.inputs = {}
         for name,attr in self.config["inputs"].items():
-            inputs[name] = {"files":[]}
-            for f in FN.find_files(attr["path"]): inputs[name]["files"].extend(f for s in ST.get_items(attr["samples"]) if s in f 
+            
+            # This dictionary contains all input information. The file list 
+            # contains lists which can have more than one element in the case 
+            # of multiple channels declared in the group.
+            self.inputs[name] = {"files":[]}
+            
+            # Find all relevant files using the list of paths and filtering with the sample and channel tags.
+            for f in FN.find_files(attr["path"]): self.inputs[name]["files"].extend(f for s in ST.get_items(attr["samples"]) if s in f 
                     and FN.modus_ponens( FN.has_key("group",attr), any(c in f for c in ST.get_items(attr.get("group",False)))))
-            inputs[name]["files"] = [list(f) for j, f in groupby(inputs[name]["files"], lambda a: a.partition("_")[0])]
-
+            
+            # Group files using the first part of their names.
+            self.inputs[name]["files"] = [list(f) for j, f in groupby(self.inputs[name]["files"], lambda a: a.partition("_")[0])]
+            
+            # Add all remaining attributes.
+            self.inputs[name].update(attr)
 
     def load(self):
         #run = Run(name = "TEST")
