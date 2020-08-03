ca8ab57a5290f5c4d23dcd029d54afa9a7c9988b
Federico Scutti <fscutti@6400L-129535-M.local>
2020-08-03 17:28:30 +1000

    some store functions

--- pyrate/core/Store.py
+++ pyrate/core/Store.py
@@ -1,1 +1,15 @@
 """ Store class """
+
+def Store:
+    def __init__(self):
+        self._transient = {}
+        self._permanent = {}
+    
+    def put(self, name, item, mode="TRAN"):
+        if mode=="PERM":    self._permanent[name] = item
+        elif mode=="TRAN":  self._transient[name] = item
+    
+    def get(self, name):
+        if name in self._transient:   return self._transient[name] 
+        elif name in self._permanent: return self._permanent[name] 
+        else: print("Some error message")
