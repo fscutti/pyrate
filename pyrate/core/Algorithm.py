f1a7b23b40bebfb436dfb265ede9f7e48deac056
Federico Scutti <fscutti@6400L-129535-M.local>
2020-08-18 08:27:55 +1000

    Algorithms execute method will take a dictionary

--- pyrate/core/Algorithm.py
+++ pyrate/core/Algorithm.py
@@ -1,13 +1,21 @@
-""" Algorithm class """
-
+""" Algorithm class.
+"""
 
 class Algorithm:
     def __init__(self):
+        self._is_done = False
+        self.store = {}
+
+    def execute(self, edict):
+        """ Override this method to define algorithms.
+            edict is a dictionary.
+        """
         pass
 
-    def initialise(self):
-        pass
-    def execute(self):
-        pass
-    def finalise(self):
-        pass
+    def conditions(self):
+        return self._is_done
+
+    def stop(self):
+        self._is_done = True
+
+
