deb8a28a53b7e175621012472de67f6a9e4b934d
Federico Scutti <fscutti@6400L-129535-M.local>
2020-08-18 14:10:26 +1000

    predefined None value for dictionary in execute

--- pyrate/core/Algorithm.py
+++ pyrate/core/Algorithm.py
@@ -2,13 +2,13 @@
 """
 
 class Algorithm:
-    __slots__ = ("name", "store", "_is_done")
+    __slots__ = ["name", "store", "_is_done"]
     def __init__(self, name, store):
         self.name = name
         self.store = store
         self._is_done = False
 
-    def execute(self, config):
+    def execute(self, config = None):
         """ Override this method to define algorithms.
             config is a dictionary.
         """
