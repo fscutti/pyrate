2eaf979bcb7431809181ea3d204b7b3ddbf4dd2c
Federico Scutti <fscutti@6400L-129535-M.local>
2020-08-28 11:18:45 +1000

    rough global event loop

--- pyrate/core/Input.py
+++ pyrate/core/Input.py
@@ -7,29 +7,81 @@ class Input:
     def __init__(self, name, iterable=(), **kwargs):
         self.__dict__.update(iterable, **kwargs)
         self.name = name
-        self.inputs = {}
+        self.istore = {}
     
-    def setup(self):
+    def load(self):
         self.groups = {"group_{}".format(g):group for g, group in enumerate(self.files)}
-        for g, files in self.groups.items(): 
-            files[0] = self._init(files[0])
+        self._nfiles = len(self.groups["group_0"])
+        self._f_idx = 0
+        self._ev_idx = 0
+        self._is_finished = False
         
-        print(self.groups)
+        for g, files in self.groups.items(): 
+            files[self._f_idx] = self._init_reader(files[self._f_idx])
+        
+
+    def is_finished(self):
+        """ All events have been read at least once.
+        """
+        return self._is_finished
+
+    def _next_files(self):
+        """ Advances the pointer to the next valid group of files
+            and initialises a Reader class. This is "transforming"
+            a string to a class so it will leave a class instance 
+            as a trace of previous usage.
+        """
+        if self._f_idx < self._nfiles - 1:
+            self._f_idx += 1
+            for g, files in self.groups.items(): 
+                files[self._f_idx] = self._init_reader(files[self._f_idx])
+
+        else: 
+            self._f_idx = -1
+
+        return self._f_idx
 
 
-    def _init(self, f):
+    def _init_reader(self, f):
+        """ Instantiate different readers here.
+        """
+        reader = None
         if isinstance(f, str):
             if f.endswith(".root"): 
-                return ReaderROOT(f)
+                reader = ReaderROOT(f,self.tree)
+            
+            elif f.endswith(".dat"): 
+                pass
+            
+            elif f.endswith(".txt"): 
+                pass
+            
+            reader.load()
+        
         else: print("ERROR: trying to reinitialise reader")
-
-
-    #def load(self):
-    #    pass
+        return reader
 
     def get_next_event(self):
-        pass
-    
+
+        self._ev_idx += 1
+        for g, files in self.groups.items(): 
+            if files[self._f_idx].get_next_event() < 0:
+                if self._next_files() < 0:
+                    self._ev_idx = -1
+                    return self._ev_idx
+        return self._ev_idx
+
+
+
+    def get_ev_idx(self):
+        if self._ev_idx:
+            return self._ev_idx
+        else:
+            print("ERROR event index not defined")
+
+
+
+
     def get_previous_event(self):
         pass
 
--- pyrate/readers/ReaderROOT.py
+++ pyrate/readers/ReaderROOT.py
@@ -4,27 +4,38 @@ import ROOT as R
 import numpy as np
 
 class ReaderROOT:
-    __slots__ = ["f", "treename"]
+    __slots__ = ["f","treename","_tree","_nevents","_is_loaded","_idx","istore"]
     def __init__(self, f, treename = None):
         self.f = f
         self.treename = treename
-    
+        self._is_loaded = False
+
     def load(self, istore = None):
         self.f = R.TFile.Open(self.f)
         if istore: self.istore = istore
         if self.treename:
             self._tree = self.f.Get(self.treename)
             self._nevents = self._tree.GetEntries()
-            self.idx = 0
+            self._idx = 0
+            self._tree.GetEntry(self._idx)
+        self._is_loaded = True
 
     def get_next_event(self):
+        """ If the next event reading will not be valid it outputs -1.
+        """
         
-        if self.idx < self._nevents: 
+        if self._idx < self._nevents - 1: 
+            self._idx += 1
             self._tree.GetEntry(self._idx)
-            self.idx += 1
-            return self.idx
+            return self._idx
         else:
             return -1
+
+    def get_idx(self):
+        if self._idx:
+            return self._idx 
+        else: 
+            print("ERROR: tree not loaded")
 
     def get_previous_event(self):
         pass
@@ -42,6 +53,7 @@ class ReaderROOT:
         else: 
             self.istore[name] = self.f.Get(name)
    
-    
+    def is_loaded(self):
+        return self._is_loaded
 
 
