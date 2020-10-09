d6ccccfe909367ccacfb268a95134a06e5b06dd8
Federico Scutti <fscutti@6400L-129535-M.local>
2020-10-09 11:19:49 +1100

    draft of selection for Region

--- pyrate/algorithms/regions/Region.py
+++ pyrate/algorithms/regions/Region.py
@@ -1,5 +1,6 @@
 """ Standard Region algorithm.
 """
+import sys
 
 from pyrate.core.Algorithm import Algorithm
 
@@ -11,13 +12,71 @@ class Region(Algorithm):
         super().__init__(name, store, logger)
 
     def execute(self, config):
+        """Computes region weight. If selection criteria are
+        not satisfied the loop is interrupted.
+        """
 
-        triggerTime = self.store.get("EVENT:SmallMuon:EventData:TriggerTime")
-        ch0RawWaveform = self.store.get("EVENT:SmallMuon:Channel_0:RawWaveform")
-        startTime = self.store.get("EVENT:RunMetadata:StartTime")
-        self.logger.info(
-            f"This is the start time: {startTime} from {self.name} for {config['name']}"
-        )
+        AND = 1
+        and_selection = config["algorithm"]["selection"]
+
+        for and_s in and_selection:
+
+            OR = 0
+            or_selection = and_s.split("||")
+
+            for or_s in or_selection:
+
+                variable_name, symbol, cut_value = self.get_selection(or_s)
+                variable_value = self.store.get(variable_name)
+
+                x, symbol, y = self.get_selection(or_s)
+
+                try:
+                    x = eval(x)
+
+                except NameError:
+                    x = self.store.get(x)
+
+                try:
+                    y = eval(y)
+
+                except NameError:
+                    y = self.store.get(y)
+
+                OR = eval(f"{x} {symbol} {y}")
+
+                if OR == 1:
+                    break
+
+            AND *= OR
+
+            if AND == 0:
+                break
+
+        region = AND
+
+        if region:
+            weights = config["algorithm"]["weights"]
+
+            for w in weights:
+                weight_value = self.store.get(w)
+                region *= weight_value
+
+        self.store.put(config["name"], region)
+
+    def get_selection(self, selection):
+        """Breaks down the selection criterion."""
+        if " " in selection:
+            selection = selection.replace(" ", "")
+
+        for check in [">=", "<=", ">", "<", "==", "!="]:
+
+            a, symbol, b = selection.partition(check)
+
+            if symbol:
+                return a, symbol, b
+
+        sys.exit(f"ERROR: no symbol found for {selection}")
 
 
 # EOF
