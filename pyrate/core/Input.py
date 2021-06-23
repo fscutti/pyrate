12276e5867a4464f3f2f9cb812f883970c2d1c00
Mike <mmews@student.unimelb.edu.au>
2021-06-23 16:21:26 +1000

    Automated reader choosing for pyrate. Added feature to let the user specify the reader

--- pyrate/core/Input.py
+++ pyrate/core/Input.py
@@ -5,6 +5,8 @@ to read from a database, synchronously or not, wrt to files.
 """
 import os
 import sys
+import mmap
+from importlib import import_module
 
 from pyrate.core.Reader import Reader
 from pyrate.readers.ReaderROOT import ReaderROOT
@@ -61,7 +63,10 @@ class Input(Reader):
                     g_files = [g_files]
 
                 self.groups[g_names[g_idx]] = g_files
-                self._set_group_reader(g_names[g_idx], self._f_idx)
+                if hasattr(self, "reader"):
+                    self._set_group_reader(g_names[g_idx], self._f_idx, self.reader)
+                else:
+                    self._set_group_reader(g_names[g_idx], self._f_idx)
 
         else:
             self._set_db_reader(self.database)
@@ -90,11 +95,12 @@ class Input(Reader):
     def _read_from_groups(self, name):
         for g_name, g_readers in self.groups.items():
 
-            # if a group variable is required then transform the name
+            # Make sure we're in the group we want to get the info from
             if "GROUP:" in name:
                 req_g_name = name.split(":")
 
                 if not g_name == req_g_name[2]:
+                    # Not the right group, skip this group
                     continue
 
             if name.startswith("INPUT:"):
@@ -108,6 +114,22 @@ class Input(Reader):
             elif name.startswith("EVENT:"):
                 self._set_group_reader(g_name, self._f_idx)
                 g_readers[self._f_idx].read(name)
+                
+            elif name.startswith("READER:"):
+                variable = name.split(":")[-1]
+                if variable == "name":
+                    self.store.put(name, g_readers[self._f_idx].__class__.__name__, "TRAN")
+                elif variable == "slots":
+                    self.store.put(name, g_readers[self._f_idx].__slots__, "TRAN")
+                elif variable == "module":
+                    self.store.put(name, g_readers[self._f_idx].__class__.__module__, "TRAN")
+                else:
+                    sys.exit(f"ERROR: Invalid reader variable '{variable}'\nError occured when attempting to get '{name}' from the store.")
+            
+            elif name.startswith("GROUPS"):
+                # Adds the group keys to the store so the user can know
+                # what groups are in use without checking the config
+                self.store.put(name, list(self.groups.keys()))
 
     def _read_from_database(self, name):
         self.db.read(name)
@@ -298,7 +320,7 @@ class Input(Reader):
 
         self.db.load()
 
-    def _set_group_reader(self, g_name, f_idx):
+    def _set_group_reader(self, g_name, f_idx, reader=None):
         """Instantiate different readers here. If the instance exists nothing
         is done. This function transforms a string into a reader.
         """
@@ -307,33 +329,131 @@ class Input(Reader):
             r_name = "_".join([g_name, str(f_idx)])
 
             f_name = self.groups[g_name][f_idx]
+            
+            if reader:
+                # The user has specified the reader they want to use
+                # Try to import the reader class
+                reader_module = "pyrate.readers."+reader
+                try:
+                    ReaderModule = import_module(reader_module)
+                    ReaderClass = ReaderModule.__getattribute__(reader)
+                except ImportError as err:
+                    sys.exit(f"Error: {err}\n Unable to import reader '{reader}' from module '{reader_module}'\n"
+                            "Check that the reader is in pyrate/readers, that the class and module have the same name, and is added the nearest __init__.py")
 
-            if f_name.endswith(".root"):
-                reader = ReaderROOT(
-                    r_name, self.store, self.logger, f_name, self.structure
-                )
-
-            elif f_name.endswith(".dat"):
-                # choose the reader based on file size.
-
-                if "sabre" in os.path.basename(f_name):
+                self.logger.info(f"User-set reader for {f_name} to {reader}")
+                reader = ReaderClass(r_name, self.store, self.logger, f_name, self.structure)
+            else: 
+                if f_name.endswith(".root"):
+                    self.logger.info(f"Auto-setting reader for {f_name} to ReaderROOT")
+                    reader = ReaderROOT(
+                        r_name, self.store, self.logger, f_name, self.structure
+                    )
+                
+                elif self.is_bt(f_name):
+                    # BlueTongue file
+                    self.logger.info(f"Auto-setting reader for {f_name} to ReaderBlueTongueMMAP")
                     reader = ReaderBlueTongueMMAP(
+                            r_name, self.store, self.logger, f_name, self.structure
+                        )
+
+                elif self.is_wd_ascii(f_name):
+                    # WaveDump file
+                    self.logger.info(f"Auto-setting reader for {f_name} to ReaderWaveDumpMMAP")
+                    reader = ReaderWaveDumpMMAP(
                         r_name, self.store, self.logger, f_name, self.structure
                     )
-
-                else:
+                elif self.is_wc_ascii(f_name):
+                    # WaveCatcher file
+                    self.logger.info(f"Auto-setting reader for {f_name} to ReaderWaveCatcherMMAP")
                     reader = ReaderWaveCatcherMMAP(
-                        r_name, self.store, self.logger, f_name, self.structure
-                    )
+                            r_name, self.store, self.logger, f_name, self.structure
+                        )
 
-            elif f_name.endswith(".txt"):
-                reader = ReaderWaveDumpMMAP(
-                    r_name, self.store, self.logger, f_name, self.structure
-                )
-
+                if not reader:
+                    # Uh oh, we haven't got a reader yet!
+                    self.logger.info(f"Unable to auto-set reader")
+                    sys.exit("ERROR: pyrate could not determine file type")
+            
             reader.load()
-
             self.groups[g_name][f_idx] = reader
 
+    def is_wc_ascii(self, filepath):
+        """ Determines if a file is a text wavecatcher file
+        """
+        filepath = os.path.abspath(filepath)
+        try:
+            with open(filepath) as f:
+                first_line = f.readline()
+            if "=== DATA FILE SAVED WITH SOFTWARE VERSION:" in first_line:
+                return True
+        except UnicodeDecodeError:
+            # Not ascii
+            pass
+        return False
+    
+    def is_wd_ascii(self, filepath):
+        """ Determines if a file is an ASCII wavedump file
+        """
+        filepath = os.path.abspath(filepath)
+        try:
+            with open(filepath) as f:
+                first_line = f.readline()
+                if "Record Length:" in first_line:
+                    return True
+        except UnicodeDecodeError:
+            pass
+        return False
+    
+    def is_bt(self, filepath):
+        """ Determines if a file is a bluetongue file
+        """
+        filepath = os.path.abspath(filepath)
+        try:
+            reader = ReaderBlueTongueMMAP(
+                        "BT_CHECK", self.store, self.logger, filepath, self.structure
+                    )
+            # Read like the BlueTongue reader does 
+            # --------------------------------------------------------------------
+            # Mimic the load() function without getting the file size as it's slow
+            reader.is_loaded = True
+            reader.f = open(reader.f, "rb")
+            reader._idx = 0
+            reader._mmf = mmap.mmap(reader.f.fileno(), length=0, access=mmap.ACCESS_READ)
+            reader._event = 0
+            reader._event_size = 0
+            reader._header_size = 0
+            # Hack to check if the board number is too high
+            # if it's over 64, then something is really wrong
+            current_pos = reader._mmf.tell()
+            reader._hd = {"n_boards": reader._get_items(1, "I")[0]}
+            if reader._hd["n_boards"] > 64:
+                # Uh oh, something's not right, probably not a BT file
+                return False
+            reader._mmf.seek(current_pos)
+            reader._set_header_dict()
+            reader._set_event_dict()
+            reader.f.close()
+            # load() function over
+            # ------------------------------------------------------------------
+            if reader._event != reader._idx * reader._event_size + reader._header_size:
+                reader._event = reader._idx * reader._event_size + reader._header_size
+            reader._move(reader._event)
+            t = FN.grab("check_word", reader._ev)
+            items_number, items_type, items_offset = t[0], t[1], t[2]
+            reader._mmf.seek(items_offset, 1)
+            cw = reader._get_items(items_number, items_type)[0]
+            if cw == 65226:
+                # All good!
+                return True
+        except:
+            pass
+        finally:
+            try:
+                reader.offload()
+                del reader
+            except:
+                pass
+        return False
 
 # EOF
