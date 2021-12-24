f582ff269ac41bf80a942fe42d55828b4d8d3def
Mike <mmews@student.unimelb.edu.au>
2021-12-24 22:26:31 +1100

    TimeDifference v1

---
+++ pyrate/algorithms/variables/TimeDifference.py
@@ -0,0 +1,41 @@
+""" Calculates the difference between two times
+    dt = time2 - time1
+    Where typically time1 is the initial time, and time2 would be the final
+
+    Required parameters:
+        time1: (float) The first time (object)
+        time2: (float) The second time (object)
+    
+    Required states:
+        execute:
+            input: <Time 1 object>, <Time 2 object>
+            output: SELF
+    
+    Example config:
+
+    TimeDifference_CH01:
+        algorithm:
+            name: TimeDifference
+        execute:
+            input: PulseTime_CH0, PulseTime_CH1
+        time1: PulseTime_CH0
+        time2: PulseTime_CH1
+"""
+
+from pyrate.core.Algorithm import Algorithm
+
+class TimeDifference(Algorithm):
+    __slots__ = ()
+
+    def __init__(self, name, store, logger):
+        super().__init__(name, store, logger)
+
+    def execute(self, config):
+        """ Caclulates the time difference time2 - time1
+        """
+        t1 = self.store.get(config["time1"])
+        t2 = self.store.get(config["time2"])
+        TimeDiff = t2-t1
+        self.store.put(config["name"], TimeDiff)
+
+# EOF
