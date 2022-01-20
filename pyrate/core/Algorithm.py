5d443c2f16f0779998575adf5e3a266982ca71b4
Federico Scutti <fscutti@6400L-129535-M.local>
2022-01-20 18:13:40 +1100

    cleanup docstrings

--- pyrate/core/Algorithm.py
+++ pyrate/core/Algorithm.py
@@ -5,6 +5,18 @@
 *************************************************************
 self.name is the name of the object you want to compute.
 self.config is the configuration dictionary as in .yaml files
+
+To define algorithms override at least one among:
+   * initialise
+   * execute 
+   * finalise
+
+At initialise you implicitly 'put' on the PERM store.
+At execute you implicitly 'put' on the TRAN store.
+At finalise you implicitly 'put' on the PERM store.
+
+The 'get' method looks across all stores at all states.
+
 *************************************************************
 """
 from pyrate.utils import strings as ST
@@ -22,43 +34,36 @@ class Algorithm:
         self.logger = logger
 
     def initialise(self):
-        """Override this method to define algorithms.
-        At this stage the method knows the current input.
-        """
+        """At this stage the method knows the current input."""
         self.store.put(self.name, enums.Pyrate.NONE, "TRAN")
 
     def execute(self):
-        """Override this method to define algorithms.
-        At this stage the method knows the current input and current event.
-        """
+        """At this stage the method knows the current input and current event."""
         self.store.put(self.name, enums.Pyrate.NONE, "TRAN")
 
     def finalise(self):
-        """Override this method to define algorithms.
-        At this stage the method knows the current input.
-        """
+        """At this stage the method knows the current input."""
         self.store.put(self.name, enums.Pyrate.NONE, "TRAN")
 
     def _prepare_input(self, state):
-        """Prepares objects on the store before the execution of the state methods.
-        """
+        """Prepares objects on the store before the execution of the state methods."""
         for o in self.config["dependency"][state]:
             self.store.get(o)
 
     def _initialise(self):
-
+        """Do not override this method!"""
         self.store.put(self.name, enums.Pyrate.NONE, "TRAN")
 
         self._prepare_input("initialise")
 
     def _execute(self):
-
+        """Do not override this method!"""
         self.store.put(self.name, enums.Pyrate.NONE, "TRAN")
 
         self._prepare_input("execute")
 
     def _finalise(self):
-
+        """Do not override this method!"""
         self.store.put(self.name, enums.Pyrate.NONE, "TRAN")
 
         self._prepare_input("finalise")
