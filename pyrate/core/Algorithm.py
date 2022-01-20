91c1c1f15389d7a41a8826d90907d1ac5dfebcb2
Federico Scutti <fscutti@6400L-129535-M.local>
2022-01-20 18:02:53 +1100

    additional docstrings

--- pyrate/core/Algorithm.py
+++ pyrate/core/Algorithm.py
@@ -3,7 +3,8 @@
 
 """
 *************************************************************
-config['name'] is the name of the object you want to compute.
+self.name is the name of the object you want to compute.
+self.config is the configuration dictionary as in .yaml files
 *************************************************************
 """
 from pyrate.utils import strings as ST
@@ -21,27 +22,25 @@ class Algorithm:
         self.logger = logger
 
     def initialise(self):
-        """Override this method to define algorithms. config is a dictionary.
+        """Override this method to define algorithms.
         At this stage the method knows the current input.
         """
         self.store.put(self.name, enums.Pyrate.NONE, "TRAN")
 
     def execute(self):
-        """Override this method to define algorithms. config is a dictionary.
+        """Override this method to define algorithms.
         At this stage the method knows the current input and current event.
         """
         self.store.put(self.name, enums.Pyrate.NONE, "TRAN")
 
     def finalise(self):
-        """Override this method to define algorithms. config is a dictionary.
+        """Override this method to define algorithms.
         At this stage the method knows the current input.
         """
         self.store.put(self.name, enums.Pyrate.NONE, "TRAN")
 
     def _prepare_input(self, state):
         """Prepares objects on the store before the execution of the state methods.
-        N.B.: config might not have a state and/or input fields defined. In this
-        case, the KeyError exception is caught and the function simply returns.
         """
         for o in self.config["dependency"][state]:
             self.store.get(o)
