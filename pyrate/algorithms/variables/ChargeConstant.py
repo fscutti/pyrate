40653c326a1897684c7767963f7f81cb8fb181b1
Mike <mmews@student.unimelb.edu.au>
2022-02-03 13:27:59 +1100

    ChargeConstant v1

--- pyrate/algorithms/variables/ChargeConstant.py
+++ pyrate/algorithms/variables/ChargeConstant.py
@@ -1,8 +1,5 @@
-""" Calculates the charge - the sum over a region of a waveform scaled by 
-    physical PMT and circuit parameters.
-    Sums the waveform over a passed in window object. The charge constant is
-    calculated from the impedance, the sample rate, waveform units, and a 
-    conversion for the desired output charge units.
+""" The charge constant is calculated from the impedance, the sample rate, 
+    waveform units, and a  conversion for the desired output charge units.
     Charge = 1/(Z * Sample rate) * Sum trace[i]
 
     Required parameters:
@@ -15,32 +12,22 @@
                        Accepts strings: e.g. V or mV
                        Otherwise accepts floats for the appropriate conversion
                        for non-physical waveforms (ADC).
-        waveform: The waveform to caluclate the charge of (typically physcial)
-        window: (tuple) The start and stop window for calculating the charge
     
     Required states:
         initialise:
             output:
-        execute:
-            input: <Waveform object>, <Window object>
-            output: SELF
     
     Example config:
     
-    Charge_CHX:
+    ChargeConstant_CHX:
         algorithm:
-            name: Charge
+            name: ChargeConstant
             impedance: 50
             rate: 500e6
             unit: pC
             waveform_unit: mV
         initialise:
             output:
-        execute:
-            input: CorrectedWaveform_CHX, Window_CHX
-            output: SELF
-        waveform: CorrectedWaveform_CHX
-        window: Window_CHX
 """
 
 import sys
@@ -49,7 +36,7 @@ from pyrate.core.Algorithm import Algorithm
 wf_units = {"V":1.0, "mV":1e-3, "uV":1e-6}
 q_units = {"C":1.0, "mC":1e3, "uC":1e6, "nC":1e9, "pC":1e12, "fC":1e15}
 
-class Charge(Algorithm):
+class ChargeConstant(Algorithm):
     __slots__ = ()
 
     def __init__(self, name, store, logger):
@@ -68,7 +55,7 @@ class Charge(Algorithm):
             try:
                 charge_units = float(charge_units)
             except:
-                sys.exit("ERROR: In algorithm Charge, unit parameter could not be converted to a float.")
+                sys.exit("ERROR: In algorithm ChargeConstant, unit parameter could not be converted to a float.")
         
         waveform_units = config["algorithm"]["waveform_unit"]
         if waveform_units in wf_units:
@@ -77,25 +64,9 @@ class Charge(Algorithm):
             try:
                 waveform_units = float(waveform_units)
             except:
-                sys.exit("ERROR: In algorithm Charge, waveform_unit could not be converted to a float.")
+                sys.exit("ERROR: In algorithm ChargeConstant, waveform_unit could not be converted to a float.")
 
         charge_constant = waveform_units * charge_units/(impedance * sample_rate)
-        self.store.put(f"{config['name']}:charge_constant", charge_constant)
-
-    def execute(self, config):
-        """ Calculates the charge by summing over the waveform
-        """
-        window = self.store.get(config["window"])
-        # check for invalid windows
-        if window == -999 or window is None:
-            Charge = -999
-        else:
-            # good to go, let's get the charge constant
-            charge_constant = self.store.get(f"{config['name']}:charge_constant")
-
-            waveform = self.store.get(config["waveform"])
-            # Calcualte the actual charge over the window
-            Charge = sum(waveform[window[0]:window[1]]) * charge_constant
-        self.store.put(config["name"], Charge)
+        self.store.put(f"{config['name']}", charge_constant)
 
 # EOF
