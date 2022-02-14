711064cc9de46a72a8d839308ce3578c858ea6ce
Lachlan Milligan <lmilligan@student.unimelb.edu.au>
2022-02-15 10:28:05 +1100

    Some bug fixes

--- pyrate/algorithms/variables/__init__.py
+++ pyrate/algorithms/variables/__init__.py
@@ -3,7 +3,6 @@ from pyrate.algorithms.variables.Baseline import Baseline
 from pyrate.algorithms.variables.Charge import Charge
 from pyrate.algorithms.variables.Sum import Sum
 from pyrate.algorithms.variables.ChargeConstant import ChargeConstant
-from pyrate.algorithms.variables.Waveform import Waveform
 from pyrate.algorithms.variables.Weight import Weight
 from pyrate.algorithms.variables.Trigger import Trigger
 from pyrate.algorithms.variables.TestingReader import TestingReader
