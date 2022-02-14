c487577e7b11c32a55666c286cb66569ff59af31
Mike <mmews@student.unimelb.edu.au>
2022-02-14 17:17:56 +1100

    updated to new structure

--- pyrate/algorithms/variables/TrapezoidFilter.py
+++ pyrate/algorithms/variables/TrapezoidFilter.py
@@ -36,46 +36,32 @@
 from pyrate.core.Algorithm import Algorithm
 
 class TrapezoidFilter(Algorithm):
-    __slots__ = ()
+    __slots__ = ('rise', 'gap', 'period', 'tau', 'zeropole', 'length')
 
-    def __init__(self, name, store, logger):
-        super().__init__(name, store, logger)
+    def __init__(self, name, config, store, logger):
+        super().__init__(self, name, config, store, logger)
     
-    def initialise(self, config):
+    def initialise(self):
         """ Set up the trapezoid parameters
         """        
         # Trapezoid parameters
-        rise = int(config["algorithm"]["rise"])
-        gap = int(config["algorithm"]["gap"])
-        period = 1/float(config["algorithm"]["rate"])
-        tau = float(config["algorithm"]["tau"])
-        if "zeropole" in config["algorithms"]:
-            zeropole = bool(config["algorithm"]["zeropole"])
+        self.rise = int(self.config["algorithm"]["rise"])
+        self.gap = int(self.config["algorithm"]["gap"])
+        self.period = 1/float(self.config["algorithm"]["rate"])
+        self.tau = float(self.config["algorithm"]["tau"])
+        if "zeropole" in self.config["algorithms"]:
+            self.zeropole = bool(self.config["algorithm"]["zeropole"])
         else:
-            zeropole = True
-        self.store.put(f"{config['name']}:rise", rise)
-        self.store.put(f"{config['name']}:gap", gap)
-        self.store.put(f"{config['name']}:period", period)
-        self.store.put(f"{config['name']}:tau", tau)
-        self.store.put(f"{config['name']}:zeropole", zeropole)
+            self.zeropole = True
+        self.length = None
 
-    def execute(self, config):
+    def execute(self):
         """ Caclulates the trap filtered waveform
         """
-        # Get the parameters
-        rise = self.store.get(f"{config['name']}:rise")
-        gap = self.store.get(f"{config['name']}:gap")
-        period = self.store.get(f"{config['name']}:period")
-        tau = self.store.get(f"{config['name']}:tau")
-        zeropole = self.store.get(f"{config['name']}:zeropole")
-
         # Get the actual waveform, finally.
-        waveform = self.store.get(config["waveform"])
-        if self.store.check(f"{config['name']}:length"):
-            length = self.store.get(f"{config['name']}:length")
-        else:
-            length = len(waveform)
-            self.store.put(f"{config['name']}:length", length, "PERM")
+        waveform = self.store.get(self.config["waveform"])
+        if self.length is None:
+            self.length = len(waveform)
 
         # Parameters and formula from Digital techniques for real-time pulse shaping in radiation measurements
         # https://doi.org/10.1016/0168-9002(94)91652-7
@@ -84,19 +70,19 @@ class TrapezoidFilter(Algorithm):
         p = []
         r = []
         trap = []
-        for i in range(length):
+        for i in range(self.length):
             # Main formula
-            d = self._v(waveform, i) - self._v(waveform, i-rise) - self._v(waveform, i-(gap + rise)) + self._v(waveform, i-(2*rise + gap))
+            d = self._v(waveform, i) - self._v(waveform, i-self.rise) - self._v(waveform, i-(self.gap + self.rise)) + self._v(waveform, i-(2*self.rise + self.gap))
             dn.append(d)
-            if zeropole:
-                M = tau/period + 0.5 # Pole-zero correction parameter
+            if self.zeropole:
+                M = self.tau/self.period + 0.5 # Pole-zero correction parameter
             else:
                 M = 1
             p.append(self._v(p, i-1) + d)
             r.append(self._v(p, i) + M*d)
-            trap.append(self._v(trap, i-1) + self._v(r, i)/(M*rise))
+            trap.append(self._v(trap, i-1) + self._v(r, i)/(M*self.rise))
         
-        self.store.put(f"{config['name']}", trap)
+        self.store.put(f"{self.name}", trap)
 
     def _v(self, waveform, i):
         """ returns the i'th element of a waveform or 0 if out of range
