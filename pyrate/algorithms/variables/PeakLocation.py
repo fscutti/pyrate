1ca937b783a46bfc695b6c7d95579d906d360102
Mike <mmews@student.unimelb.edu.au>
2022-02-14 16:47:34 +1100

    made window use optional

--- pyrate/algorithms/variables/PeakLocation.py
+++ pyrate/algorithms/variables/PeakLocation.py
@@ -23,10 +23,17 @@
 from pyrate.core.Algorithm import Algorithm
 
 class PeakLocation(Algorithm):
-    __slots__ = ()
+    __slots__ = ('use_window')
 
     def __init__(self, name, config, store, logger):
         super().__init__(name, config, store, logger)
+    
+    def initialise(self):
+        """ Allows the user to determine if the peak is in a smaller window
+        """
+        self.use_window = False
+        if "window" in self.config["algorithm"]:
+            self.use_window = bool(self.config["algorithm"]["window"])
 
     def execute(self):
         """ Caclulates the pulse time based on the mode chosen
@@ -34,8 +41,11 @@ class PeakLocation(Algorithm):
         waveform = self.store.get(self.config["waveform"])
         # Peak location is the highest point on the waveform
         # PeakLocation = np.argmax(waveform)
-        window = self.store.get(self.config["window"])
-        if window == -999 or window == None:
+        if self.use_window:
+            window = self.store.get(self.config["window"])
+        else:
+            window = (None, None)
+        if window == -999 or window is None:
             PeakLocation = -999
         else:
             PeakLocation = max(range(len(waveform[window[0]:window[1]])), key=waveform.__getitem__)
