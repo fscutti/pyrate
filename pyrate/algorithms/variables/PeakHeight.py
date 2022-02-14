eaf2391f2e2a0e5a50d0850399ca11bafd78093a
Lachlan Milligan <lmilligan@student.unimelb.edu.au>
2022-02-14 17:45:22 +1100

    Made window optional, same change to PeakLocation

--- pyrate/algorithms/variables/PeakHeight.py
+++ pyrate/algorithms/variables/PeakHeight.py
@@ -14,29 +14,39 @@
     PeakHeight_CHX:
         algorithm:
             name: PeakHeight
+            window: True
         execute:
             input: CorrectedWaveform_CHX, Window
         waveform: CorrectedWaveform_CHX
         window: Window
+
 """
 
 from pyrate.core.Algorithm import Algorithm
 
 class PeakHeight(Algorithm):
-    __slots__ = ()
-
+    __slots__ = ('use_window')
+ 
     def __init__(self, name, config, store, logger):
         super().__init__(name, config, store, logger)
+    
+    def initialise(self):
+        """ Allows the user to determine if the peak is in a smaller window
+        """
+        self.use_window = False
+        if "window" in self.config["algorithm"]:
+            self.use_window = bool(self.config["algorithm"]["window"])
 
     def execute(self):
         """ Caclulates the waveform peak height (maximum)
         """
         waveform = self.store.get(self.config["waveform"])
         window = self.store.get(self.config["window"])
-        if window == -999 or window is None:
+        if window == -999 or window == None:
             PeakHeight = -999
         else:
             PeakHeight = max(waveform[window[0]:window[1]])
+
         self.store.put(self.name, PeakHeight)
 
 # EOF
