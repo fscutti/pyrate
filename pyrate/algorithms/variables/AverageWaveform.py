8841132b66c776543e35ebd5612dc93e67dcf1e3
Mike <mmews@student.unimelb.edu.au>
2022-04-05 11:29:30 +1000

    changed len to .size

--- pyrate/algorithms/variables/AverageWaveform.py
+++ pyrate/algorithms/variables/AverageWaveform.py
@@ -81,11 +81,12 @@ class AverageWaveform(Algorithm):
 
         # Handles the case when a channel wasn't present in the event - specific to ZLE and PSD firmware which don't collect waveforms in that case
         # ToDo: How we represent missing channels may change, so this check will need to be updated in such a situation
-        if len(waveform) > 1:
-            if self.cum_waveform.shape[0] == 0:
-                RecordLength = len(waveform)
-                self.cum_waveform = np.zeros(RecordLength)
-                
+
+        # Only runs if the waveform isn't empty
+        if waveform.size > 1:
+            if self.cum_waveform.size < self.cum_waveform.size:
+                self.cum_waveform = np.zeros(waveform.size)
+
             self.cum_waveform += waveform
             # If you dont trust emax - emin
             self.nevents += 1
