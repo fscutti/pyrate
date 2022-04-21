3250b01e47613290ada0def07cd0ccda7e51af28
Mike <mmews@student.unimelb.edu.au>
2022-04-21 13:56:28 +1000

    using argmax, optional interpolation

--- pyrate/algorithms/variables/LeadingEdgeThreshold.py
+++ pyrate/algorithms/variables/LeadingEdgeThreshold.py
@@ -28,10 +28,11 @@
 
 from pyrate.core.Algorithm import Algorithm
 import numpy as np
+from code import interact
 
 
 class LeadingEdgeThreshold(Algorithm):
-    __slots__ = ("offset", "threshold")
+    __slots__ = ("offset", "threshold", "interpolate")
 
     def __init__(self, name, config, store, logger):
         super().__init__(name, config, store, logger)
@@ -41,20 +42,34 @@ class LeadingEdgeThreshold(Algorithm):
         # offset parameter decides offset from threshold crossing point
         self.offset = int(self.config["algorithm"]["offset"])
         self.threshold = self.config["algorithm"]["threshold"]
+        self.interpolate = False
+        if "interpolate" in self.config["algorithm"]:
+            self.interpolate = True
 
     def execute(self):
         """Caclulates the waveform threshold crossing point"""
         # Get the actual waveform, finally.
         waveform = self.store.get(self.config["waveform"])
 
-        threshold_cross_list = waveform[waveform > self.threshold]
-        if threshold_cross_list.size>0:
-            threshold_cross_idx = np.where(threshold_cross_list[0]==waveform)
-            LeadingEdgeTime = threshold_cross_idx[0][0] - self.offset
+        cross_index = np.argmax(waveform > self.threshold)
+        if cross_index:
+            if self.interpolate:
+                # We want to interpolate between the points
+                x1, x2 = cross_index - 1, cross_index
+                y1, y2 = waveform[x1], waveform[x2]
+                if y1 == y2:
+                    # We'll get an invalid value, just want the midpoint
+                    LeadingEdgeTime = x1 + 0.5
+                else:
+                    # Interpolation point = x1 + (y-y1)*(x2-x1)/(y2-y1) ##
+                    LeadingEdgeTime = x1 + (self.threshold-y1)/(y2-y1)
+            else:
+                # We just want the index
+                LeadingEdgeTime = cross_index
         else:
             LeadingEdgeTime = -999
 
-        self.store.put(self.name, LeadingEdgeTime)
+        self.store.put(self.name, LeadingEdgeTime-self.offset)
 
 
 # EOF
